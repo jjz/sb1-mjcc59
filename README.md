7bda792e572f542e80860ba1aac592866f88c448
jjz <woaf1003@gmail.com>
2024-10-15 07:03:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-mjcc59
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jjz/sb1-mjcc59)
---
+++ programs/token-transfer/src/lib.rs
@@ -0,0 +1,37 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod token_transfer {
+    use super::*;
+
+    pub fn transfer_token(ctx: Context<TransferToken>, amount: u64) -> Result<()> {
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+
+        token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                transfer_instruction,
+            ),
+            amount,
+        )?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct TransferToken<'info> {
+    #[account(mut)]
+    pub from: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub to: Account<'info, TokenAccount>,
+    pub authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+}
